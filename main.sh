e5ecb8683e7cf21cabcdca2e03f8917e298c01e0
gilleshenrard <gilles.henrard@hotmail.com>
2015-04-03 19:22:24 +0200

    ajout des tests de fichiers et répertoires

--- main.sh
+++ main.sh
@@ -7,7 +7,16 @@
 if [ $# -eq 0 ]
 	then
 		echo "Veuillez entrer un nom de fichier/repertoire"
+
 	elif [ ! -e $1 ]
 	then
 		echo "$1 n'existe pas"
+
+	elif [ -f $1 ]
+	then
+		echo "$1 est un fichier"
+
+	elif [ -d $1 ]
+	then
+		echo "$1 est un répertoire"
 fi
