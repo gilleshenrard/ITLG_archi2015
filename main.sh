89b2d43428e08ab2ec77b03cd795105780b47656
gilleshenrard <gilles.henrard@hotmail.com>
2015-04-03 19:19:23 +0200

    test si nom entré et si le fichier existe

--- main.sh
+++ main.sh
@@ -1,1 +1,13 @@
 #! /bin/bash
+
+#
+#	TP 1.1 - Utilisation de la fonction TEST
+#
+
+if [ $# -eq 0 ]
+	then
+		echo "Veuillez entrer un nom de fichier/repertoire"
+	elif [ ! -e $1 ]
+	then
+		echo "$1 n'existe pas"
+fi
