44776d102098966e95b3630fe093bd651c82a6dc
gilles <gilles.henrard@hotmail.com>
2015-04-04 20:59:23 +0200

    mise en une seule ligne des commandes

--- main.sh
+++ main.sh
@@ -5,11 +5,9 @@
 #
 
 # EXERCICE 1
-cut -d ':' -f 1 /etc/passwd > /tmp/out2
-sort /tmp/out2 > /tmp/out
+cut -d ':' -f 1 /etc/passwd > /tmp/out2 ; sort /tmp/out2 > /tmp/out
 rm /tmp/out2
 
 # EXERCICE 2
-ls -l /usr/bin > /tmp/out
-wc -l /tmp/out
+ls -l /usr/bin > /tmp/out ; wc -l /tmp/out
 rm /tmp/out
