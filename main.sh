bb412acdbd080895db824fadd1f48ffaea703126
gilles <gilles.henrard@hotmail.com>
2015-04-04 05:06:50 +0200

    Ecriture du menu

--- main.sh
+++ main.sh
@@ -1,1 +1,12 @@
 #! /bin/bash
+
+#
+#	TP 1.2 - Utilisation de la fonction selon que (case)
+#
+
+echo "*********** Menu général ************"
+echo "<1> Comptabilité"
+echo "<2> Gestion commerciale"
+echo "<3> Paie"
+echo "<9> Quitter"
+echo "*************************************"
