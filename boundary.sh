82e3da020f8647db7d5a467080ab144598e83318
gilles <gilles.henrard@hotmail.com>
2015-04-04 05:48:42 +0200

    Ajout du traitement avec choix des bornes

--- boundary.sh
+++ boundary.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 
 #
-#	TP 1.3 - Traitement de l'équation y=x et y=x², -10<x<10 avec un UNTIL
+#	TP 1.3 - Traitement de l'équation y=x et y=x², -10<x<10 avec choix des bornes
 #
 
-i=-10
+i=$1
 echo "*** y=x ***"
-until [ $i -gt 10 ]; do 
+until [ $i -gt $2 ]; do 
 	echo "x=$i, y=$i"
 	let i=i+1
 done
 echo "***********"
 echo
-i=-10
+i=$1
 echo "*** y=x² ***"
-until [ $i -gt 10 ]; do
+until [ $i -gt $2 ]; do
 	let j=$i*$i
 	echo "x=$i, y=$j"
 	let i=i+1
