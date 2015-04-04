c47b8223473fbe07cef29a593754bcbb0078d764
gilles <gilles.henrard@hotmail.com>
2015-04-05 00:00:45 +0200

    reponse aux questions 5 et 6
    utilisation du -o dans le sort, plutot que >

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@
 #
 
 # EXERCICE 1
-cut -d ':' -f 1 /etc/passwd > /tmp/out2 ; sort /tmp/out2 > /tmp/out
+cut -d ':' -f 1 /etc/passwd > /tmp/out2 ; sort -o /tmp/out /tmp/out2
 rm /tmp/out2
 
 # EXERCICE 2
@@ -23,3 +23,10 @@ ps -eo euser,comm | grep $1
 #le fichier /etc/passwd est lu, ligne par ligne, et le retour "\n" est remplacé par "\r", et la nouvelle ligne est copiée dans /tmp/newpasswd
 #	etant donné que "\r" ne fait pas passer le curseur à la ligne suivante, contrairement à "\n", le fichier final consiste en une seule ligne,
 #	où l'on voit la dernière ligne et les traces des anciennes lignes, plus longues
+
+# EXERCICE 6
+#partie 1
+sort -g ex6Data
+#parties 2 et 3 (les manips se font dans un fichier "test", penser à le virer)
+cp ex6Data test
+cut -d '/' -f 2 test > /tmp/test ; sort -r -o test /tmp/test
