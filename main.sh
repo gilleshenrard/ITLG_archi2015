e335ffa5ee874d70bb860e009992768531159a3f
gilles <gilles.henrard@hotmail.com>
2015-04-04 20:35:23 +0200

    ajout des exercices 1 et 2

--- main.sh
+++ main.sh
@@ -1,1 +1,15 @@
 #! /bin/bash
+
+#
+#	Atelier - exercices sur le chapitre 8
+#
+
+# EXERCICE 1
+cut -d ':' -f 1 /etc/passwd > /tmp/out2
+sort /tmp/out2 > /tmp/out
+rm /tmp/out2
+
+# EXERCICE 2
+ls -l /usr/bin > /tmp/out
+wc -l /tmp/out
+rm /tmp/out
