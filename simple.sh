7087b3ab99a236a3e6793e15fb048b313a00f820
gilles <gilles.henrard@hotmail.com>
2015-04-04 05:33:30 +0200

    Ajout du traitement pour y=x²

--- simple.sh
+++ simple.sh
@@ -4,6 +4,15 @@
 #	TP 1.3 - Traitement de l'équation y=x et y=x², -10<x<10 avec un FOR
 #
 
+echo "*** y=x ***"
 for i in `seq -10 10`
 	do echo "x=$i, y=$i"
 done
+echo "***********"
+echo
+echo "*** y=x² ***"
+for i in `seq -10 10`
+	do let j=$i*$i
+	echo "x=$i, y=$j"
+done
+echo "************"
