0336674b6f4f5b4a4abca56e9263736c66d4f5cb
gilles <gilles.henrard@hotmail.com>
2015-04-05 20:08:37 +0200

    début de piste pour l'exercice 1.5

--- main.sh
+++ main.sh
@@ -1,1 +1,14 @@
 #! /bin/bash
+
+#
+#	TP 1.5 - Utilisation de la fonction UNTIL
+#
+
+entry=''
+until [ $entry == 'Q' ]
+	do read entry
+
+	if [ $(entry) -le 0 ]; then
+		echo "Erreur pendant l'exécution"
+	fi
+done
