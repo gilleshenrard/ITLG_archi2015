9ad3d344d2edef5d8a5444960256d1b2810c6545
gilles <gilles.henrard@hotmail.com>
2015-04-05 19:47:07 +0200

    possible voie pour l'exercice 1.4.2

--- ifB.sh
+++ ifB.sh
@@ -4,10 +4,13 @@
 #	TP 1.4b - Listage des lignes dont les valeurs sont supérieures à 10
 #
 
-for line in `cat data`; do
-	num=$line
-	cut -d ' ' -f 1 $num
-	if [ $num -ge 10 ]
-		then echo "$line"
+cat data | cut -d' ' -f2 > /tmp/out
+
+while read line
+do
+	if [ $line -ge 10 ]; then
+		echo "$line"
 	fi
-done
+done < /tmp/out
+
+rm /tmp/out
