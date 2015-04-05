3cd87eadb71a5c7ab64139625d16bb492c894ca3
gilles <gilles.henrard@hotmail.com>
2015-04-05 13:49:54 +0200

    resolution exercice 18

--- main.sh
+++ main.sh
@@ -54,3 +54,9 @@ cat zola_curee.txt | grep '[1-9]' | wc -l
 
 # EXERCICE 16
 cat zola_curee.txt | grep '[^.;:/?!]$' | wc -l
+
+# EXERCICE 17
+cat zola_curee.txt | grep '^.r' | wc -l
+
+EXERCICE 18
+cat zola_curee.txt | grep '[A-Z|a-z]r' | wc -l
