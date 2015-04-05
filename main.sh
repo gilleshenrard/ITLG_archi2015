9eb4c419793f69bba4b3105e3c3d2ed75b00f551
gilles <gilles.henrard@hotmail.com>
2015-04-06 01:37:19 +0200

    resolution de l'exercice 1.5

--- main.sh
+++ main.sh
@@ -4,11 +4,14 @@
 #	TP 1.5 - Utilisation de la fonction UNTIL
 #
 
-entry=''
-until [ $entry == 'Q' ]
-	do read entry
+entry=''						#Initialisation de la variable
+until [ "$entry" == "Q" ]
+	do read entry					#Lecture de la commande
 
-	if [ $(entry) -le 0 ]; then
-		echo "Erreur pendant l'exécution"
+	if [ "$entry" != "Q" ]; then			#Test si non 'Q'
+		$entry					#Exécution de la commande
+		if [ $? -ne 0 ]; then			#Test de la valeur de retour (#?)
+			echo "Erreur à l'exécution"
+		fi
 	fi
 done
