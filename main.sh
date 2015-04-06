ae1bf7d4e9fc1ac1ea7393c92c4d3e758a63a08b
gilleshenrard <gilles.henrard@hotmail.com>
2015-04-06 17:12:23 +0200

    resolution de l'exercice 1.7 (regardé la soluce)

--- main.sh
+++ main.sh
@@ -1,1 +1,21 @@
 #! /bin/bash
+
+#
+#	TP 1.7a - Création d'un menu grâce à la fonction select
+#
+
+select choix in \
+		"Liste des utilisateurs connectés" \
+		"Liste des processus" \
+		"Informations de l'utilisateur (nom, UID, GID, TTY1)" \
+		"Quitter"
+	do
+	case $REPLY in
+		1) who;;
+		2) ps -eo ruser,comm;;
+		3) id;;
+		4) exit 0;;
+		*) echo "choix invalide";;
+	esac 
+done
+
