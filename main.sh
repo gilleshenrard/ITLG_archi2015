5a422e5a145e7cac9530a08d827e27d793384e31
gilles <gilles.henrard@hotmail.com>
2015-04-20 00:38:29 +0200

    affichage des fichiers de a à m dans /etc pour l'ex 2

--- main.sh
+++ main.sh
@@ -6,8 +6,11 @@
 
 # EXERCICE 1
 #	partie 1
-sudo cp -r /etc ~/etc ; tar -cvf ~/etc.tar ~/etc
+sudo cp -r /etc ~ ; tar -cvf ~/etc.tar ~/etc
 split -b 100K ~/etc.tar etc
 #	partie 2
 cat etc* > ~/etc.tar
 rm etc*
+
+#EXERCICE 2
+ls -l /etc | grep -e ^[a-m]
