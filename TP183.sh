7c41e427d63a7600de21fd6b24f4026dd9ab81f5
gilles <gilles.henrard@hotmail.com>
2015-04-22 21:41:07 +0200

    début de réponse pour TP 1.8.3

---
+++ TP183.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+final=$1
+
+function calc {
+	case $1 in
+		"+") let final=$final+$2;;
+		"-") let final=$final-$2;;
+		"*") let final=$final*$3;;
+		"/") let final=$final/$4;;
+		*) echo "mauvaise operande";;
+	esac
+}
+
+op="0"
+until [ $op != "=" ]; do
+	read next
+	
+done
