3454963bea3c394c2b11d9dbc46e4f7e7b54aef1
gilleshenrard <gilles.henrard@hotmail.com>
2015-05-23 14:13:43 +0200

    résolution de quelques exercices dans ex2.sh

--- ex2.sh
+++ ex2.sh
@@ -2,3 +2,13 @@
 
 #question 1
 sort telephone.txt
+
+#question 2
+wc -l telephone.txt
+#ou cat telephone.txt | wc -l si on ne veut pas le nom du fichier
+
+#question 3
+grep -i "dupond" telephone.txt
+
+#question 4
+grep -m 1 -i "dupond" telephone.txt | cut -f 3
