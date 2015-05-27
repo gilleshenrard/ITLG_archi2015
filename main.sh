8101e3e079e04414a4baf5cfe3568322f5ea3742
gilleshenrard <gilles.henrard@hotmail.com>
2015-05-27 22:31:12 +0200

    resolution exercice 2

--- main.sh
+++ main.sh
@@ -1,1 +1,22 @@
 #! /bin/bash
+
+weekago=$(date -d "1 week ago" +"%Y%m%d")
+
+#sauvegarde des derniers logs
+#retrait de reboot
+#retrait des espaces parasites
+#sauvegarde de seulement le nom, le jour et le mois
+#retrait des deux dernières lignes du log (celles qui font chier, la)
+#retrait des doublons
+#enregistrement dans /tmp/logs
+last -R | grep -v reboot | tr -s ' ' | cut -d' ' -f 1,4,5 | head -n -2 | sort -u -t' ' -k1,2 > /tmp/logs
+
+while read user month day
+do
+	lastlogin=$(date -d "$month $day" +"%Y%m%d")
+	if [ $lastlogin -le $weekago ]; then
+		tar -czvf "/home/$user/" "/home/$user.tar.gz"
+	fi
+done < /tmp/logs
+
+rm /tmp/logs
